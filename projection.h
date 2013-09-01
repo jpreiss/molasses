0da01109f7326f0d2cc338c2e9ddb6a44f826604
jp <donjeezy@gmail.com>
2013-09-01 02:30:26 -0400

    get rid of conditionally compiled test stuff in projection matrix

--- projection.h
+++ projection.h
@@ -20,22 +20,10 @@ Mat perspectiveProjection(Camera const &camera, double fov, double near, double 
 	Mat view = Mat::fromRows33(camera.x(), camera.up, camera.direction);
 	float xy = 1 / tan(fov / 2);
 
-//#define THEIRS
-
-#ifdef THEIRS
-	float nearmap = - far / (far - near);
-	float farmap  = - (far * near) / (far - near);
-	Mat projection = Mat(xy, 0,  0,       0,
-		                 0,  xy, 0,       0,
-						 0,  0,  nearmap, farmap,
-						 0,  0,  -1,       0);
-
-#else
 	Mat projection = Mat(xy, 0,  0,                0,
 		                 0,  xy, 0,                0,
 						 0,  0,  1 / (far - near), near / (far - near),
 						 0,  0,  1,                0);
-#endif
 
 	return projection * (view * toOrigin);
 }
